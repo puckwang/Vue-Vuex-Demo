9682ca59c1dd50e8571581c93564edf64aa22769
YiXiang <s9801077@gmail.com>
2017-08-15 15:36:13 +0800

    vue & vuex 04 - 使用 vue-router 建構 Single Page Application

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,23 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import Hello from '../components/Hello'
+import CtoF from '../components/CtoF'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '/hello',
       name: 'Hello',
       component: Hello
+    }, {
+      path: '/c2f',
+      name: 'c2f',
+      component: CtoF
+    }, {
+      path: '/*',
+      redirect: '/hello'
     }
   ]
 })
