e49fa1ec7832baf744c07b8348baf3c0e69ea949
YiXiang <s9801077@gmail.com>
2017-08-15 16:38:29 +0800

    vue & vuex 06 - 攝氏轉換華氏工具 - II (v-if, v-show, methods)

--- src/components/CtoF.vue
+++ src/components/CtoF.vue
@@ -1,8 +1,15 @@
 <template>
   <div>
     <h1>攝氏/華氏 轉換器</h1>
-    <h2>華氏: {{ fahrenheit }}</h2>
-    <h2>攝氏: <input type="number" v-model="celsius"/></h2>
+    <input id="switch" type="checkbox" v-model="isCelsius"/>
+    <label for="switch">切換: {{ toggleTemperature }}</label>
+
+    <p v-if="isCelsius">攝氏：{{ celsius }} °C</p>
+    <p v-if="!isCelsius">華氏：{{ fahrenheit }} °F</p>
+    <span v-show="!isCelsius">攝氏：</span>
+    <span v-show="isCelsius">華氏：</span>
+    <input type="number" v-model="userIn"/>
+    <button @click="alertResult();">Alert</button>
   </div>
 </template>
 
@@ -11,13 +18,25 @@
     name: 'CtoF',
     data() {
       return {
-        celsius: 40,
+        isCelsius: true,
+        userIn: 100,
       }
     },
     computed: {
       fahrenheit() {
-        return this.celsius * 9 / 5 + 32;
+        return this.userIn * 9 / 5 + 32;
+      },
+      celsius() {
+        return (this.userIn - 32) * 5 / 9;
+      },
+      toggleTemperature () {
+        return this.isCelsius ? "攝氏 轉換 華氏" : "華氏 轉換 攝氏";
       }
+    },
+    methods: {
+        alertResult() {
+            alert(this.isCelsius ? '攝氏' + this.celsius : '華氏' + this.fahrenheit);
+        }
     }
   }
 </script>
