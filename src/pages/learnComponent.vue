b3e6648f9712e6af1580b8a09b1eb1e8b4a9ff67
YiXiang <s9801077@gmail.com>
2017-08-16 16:48:10 +0800

    vue & vuex 08 - component - II (v-for 小幫手解決重複元件)

--- src/pages/learnComponent.vue
+++ src/pages/learnComponent.vue
@@ -2,7 +2,7 @@
   <div>
     <NavBar></NavBar>
     <JumBotron></JumBotron>
-    <Container></Container>
+    <Container :list="list"></Container>
   </div>
 </template>
 <script>
@@ -12,6 +12,27 @@
 
   export default {
     name: 'learnComponent',
+    data() {
+      return {
+        list: [
+          {
+            title: 'Heading 1',
+            info: 'Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. ',
+            btntext: 'View details »'
+          },
+          {
+            title: 'Heading 2',
+            info: 'Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. ',
+            btntext: 'View details »'
+          },
+          {
+            title: 'Heading 3',
+            info: 'Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.',
+            btntext: 'View details »'
+          },
+        ]
+      }
+    },
     components: {
       NavBar,
       JumBotron,
